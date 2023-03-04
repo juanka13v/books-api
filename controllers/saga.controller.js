5bcddb531a3e9f5fcf80e6df7fd7d34c583a5e87
juanka13v <86644115+juanka13v@users.noreply.github.com>
2023-03-04 19:48:54 +0000

    update saga controller

--- controllers/saga.controller.js
+++ controllers/saga.controller.js
@@ -1,77 +1,52 @@
 const Saga = require('../models/Saga')
-
+const { StatusCodes } = require('http-status-codes')
+const { NotFoundError } = require('../errors')
 
 const getAllSagas = async (req, res, next) => {
-    try {
-        const sagas = await Saga.find()
+    const sagas = await Saga.find()
 
-        if (!sagas) {
-            res.status(404).json({ status: 'error', message: 'sagas not found' })
-        }
+    if (sagas.length < 1) throw new NotFoundError(`No sagas found`)
 
-        res.status(200).json({ status: 'success', sagas })
-
-    } catch (err) {
-        next(err)
-    }
-
+    res.status(StatusCodes.OK).json({ status: 'success', sagas })
 };
 
 const getSaga = async (req, res, next) => {
     const { id } = req.params;
-
-    try {
-        const saga = await Saga.findById(id);
+    const saga = await Saga.findById(id);
 
 
-        if (!saga)
-            return res.status(404).json({ status: "error", message: "saga not found" });
+    if (!saga) throw new NotFoundError(`No saga with id: ${id}`)
 
-        res.status(200).json({ status: 'success', saga })
-    } catch (err) {
-        next(err)
-    }
+    res.status(StatusCodes.OK).json({ status: 'success', saga })
 };
 
 const createSaga = async (req, res, next) => {
     const newSaga = new Saga(req.body);
+    const saveSaga = await newSaga.save();
 
-    try {
-        const saveSaga = await newSaga.save();
-        res.status(201).json({ status: "created", saga: saveSaga });
-    } catch (err) {
-        next(err)
-    }
+    res.status(StatusCodes.CREATED).json({ status: "created", saga: saveSaga });
 };
 
 const deleteSaga = async (req, res, next) => {
     const { id } = req.params;
+    const deletedSaga = await Saga.findByIdAndDelete(id);
 
-    try {
-        const deletedSaga = await Saga.findByIdAndDelete(id);
-        if (!deletedSaga) {
-            return res.status(404).json({ status: "error", message: "saga not exist" });
-        }
+    if (!deletedSaga) throw new NotFoundError(`No saga with id ${id}`)
 
-        res.status(202).json({ status: "success", message: "saga eliminated" });
-    } catch (err) {
-        next(err)
-    }
-
+    res.status(StatusCodes.OK).json({ status: "success", message: "saga eliminated" });
 };
 
 const updateSaga = async (req, res) => {
     const { id } = req.params;
     const update = req.body;
 
-    try {
-        const updatedSaga = await Saga.findByIdAndUpdate(id, update, {
-            new: true,
-        });
-        res.status(202).json({ status: "success", saga: updatedSaga });
-    } catch (err) {
-        next(err)
-    }
+    const updatedSaga = await Saga.findByIdAndUpdate(id, update, {
+        new: true,
+    });
+
+    if (!updatedSaga) throw new NotFoundError(`No saga with id ${id}`)
+
+    res.status(StatusCodes.OK).json({ status: "success", saga: updatedSaga });
 };
 
 module.exports = {
--- models/Saga.js
+++ models/Saga.js
@@ -6,6 +6,10 @@ const sagaSchema = new mongoose.Schema(
             type: String,
             required: true,
         },
+        author: {
+            type: String,
+            required: true,
+        },
         description: {
             type: String,
         },
